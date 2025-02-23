38d6dd191bf16f07fdde496bba72b0011db1cf4a
django123 <edougajean@gmail.com>
2025-02-23 15:00:08 +0100

    add springdoc dependencies to document api with openApi

---
+++ accounts/src/main/java/io/django/accounts/config/OpenApiConfig.java
@@ -0,0 +1,32 @@
+package io.django.accounts.config;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Accounts microservice REST API Documentation",
+                description = "EazyBank Accounts microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "EDOUGA JEAN PATRICK",
+                        email = "edougajean@gmail.com",
+                        url = ""
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = ""
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description =  "EazyBank Accounts microservice REST API Documentation",
+                url = "localhost:8083/swagger-ui.html"
+        )
+)
+public class OpenApiConfig {
+}
